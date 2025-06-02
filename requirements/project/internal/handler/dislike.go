c96b3a1ef6176841c58bb2e994f116bbb333ff56
elmehdibelfkih <elmehdibelfkih@gmail.com>
2025-06-02 03:41:13 +0100

    saving 2025-06-02 fragment

--- requirements/project/internal/handler/dislike.go
+++ requirements/project/internal/handler/dislike.go
@@ -30,6 +30,6 @@ func DislikeHandler(w http.ResponseWriter, r *http.Request) {
 		forumerror.InternalServerError(w, r, err)
 		return
 	}
-	link := fmt.Sprintf("/#%d", postId)
+	link := fmt.Sprintf("%s#%d", r.Header.Get("Referer"), postId)
 	http.Redirect(w, r, link, http.StatusSeeOther)
 }
